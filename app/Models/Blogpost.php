6a2488f1f7bc76c1006679a3f1567d86e9035f8b
Jesper Bertijn <bert0018@hz.nl>
2022-07-11 11:49:41 +0200

    Added semicolon

--- app/Models/Blogpost.php
+++ app/Models/Blogpost.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Blogpost extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'excerpt', 'body']
+    protected $fillable = ['title', 'excerpt', 'body'];
 }
